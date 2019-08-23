054f6d5549cef9e8cd6e8cb6859c23db42461860
Bill13579 <35489013+Bill13579@users.noreply.github.com>
2019-08-23 12:07:35 -0700

    Display checkmark after completion of "Save for Later"

--- src/popup/event-listeners/recorder.js
+++ src/popup/event-listeners/recorder.js
@@ -1,7 +1,16 @@
 import { lastRecord, record, restore } from "../recorder"
 
+let saveForLaterBtn = document.getElementById("save-for-later");
+let sflTimeout = () => {
+    saveForLaterBtn.removeAttribute("done");
+};
 export function saveForLater() {
-    record();
+    saveForLaterBtn.setAttribute("disabled", "");
+    record().then(() => {
+        saveForLaterBtn.removeAttribute("disabled");
+        saveForLaterBtn.setAttribute("done", "");
+        clearTimeout(sflTimeout); setTimeout(sflTimeout, 2000);
+    });
 }
 
 export { restore }
