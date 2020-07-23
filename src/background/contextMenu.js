0979db274ab01a83b766f4842e8e096a869287cd
Bill Kudo <bluesky42624@gmail.com>
2020-07-22 20:31:04 -0700

    Don't update the context menu at the same time; define contexts

--- src/background/contextMenu.js
+++ src/background/contextMenu.js
@@ -1,41 +1,54 @@
 import G from "./globals"
 
+let updating = false;
+let callback = undefined;
+
 export async function updateContextMenu() {
-    await browser.contextMenus.removeAll();
-    await browser.contextMenus.create({
-        id: "tabby-send-tab-to",
-        title: "Send Tab to...",
-        contexts: ["all"],
-    });
-    let windows = await browser.windows.getAll({
-        populate: false,
-        windowTypes: ["normal"],
-    });
-    for (let i = 0; i < windows.length; i++) {
-        let windowId = windows[i].id;
+    if (!updating) {
+        updating = true;
+        await browser.contextMenus.removeAll();
+        await browser.contextMenus.create({
+            id: "tabby-send-tab-to",
+            title: "Send Tab to...",
+            contexts: ["audio", "editable", "frame", "image", "link", "page", "password", "selection", "tab", "video"],
+        });
+        let windows = await browser.windows.getAll({
+            populate: false,
+            windowTypes: ["normal"],
+        });
+        for (let i = 0; i < windows.length; i++) {
+            let windowId = windows[i].id;
+            await browser.contextMenus.create({
+                parentId: "tabby-send-tab-to",
+                title: G.windowProperties.hasOwnProperty(windowId.toString()) ? G.windowProperties[windowId.toString()].name : "Window " + (i+1),
+                onclick: (info, tab) => {
+                    browser.tabs.move(tab.id, {
+                        windowId: parseInt(windowId),
+                        index: -1
+                    });
+                }
+            });
+        }
         await browser.contextMenus.create({
             parentId: "tabby-send-tab-to",
-            title: G.windowProperties.hasOwnProperty(windowId.toString()) ? G.windowProperties[windowId.toString()].name : "Window " + (i+1),
+            type: "separator"
+        });
+        await browser.contextMenus.create({
+            parentId: "tabby-send-tab-to",
+            title: "A New Window",
             onclick: (info, tab) => {
-                browser.tabs.move(tab.id, {
-                    windowId: parseInt(windowId),
-                    index: -1
+                browser.windows.create({
+                    tabId: tab.id,
+                    incognito: tab.incognito
                 });
             }
         });
-    }
-    await browser.contextMenus.create({
-        parentId: "tabby-send-tab-to",
-        type: "separator"
-    });
-    await browser.contextMenus.create({
-        parentId: "tabby-send-tab-to",
-        title: "A New Window",
-        onclick: (info, tab) => {
-            browser.windows.create({
-                tabId: tab.id,
-                incognito: tab.incognito
-            });
+        updating = false;
+        if (callback) {
+            callback();
+            callback = undefined;
         }
-    });
+    } else {
+        callback = updateContextMenu;
+    }
 }
