b3e8ad0c4021b872174e9b473d90db40668fff96
Bill Kudo <bluesky42624@gmail.com>
2022-08-01 18:48:03 -0700

    Merge new version repository (Part 2)

--- src/popup/globals.js
+++
@@ -1,9 +0,0 @@
-import "Polyfill"
-
-const globals = {
-    tabsList: undefined,
-    isSelecting: false,
-    hideAfterTabSelection: undefined,
-    searchInURLs: undefined
-};
-export default globals;
