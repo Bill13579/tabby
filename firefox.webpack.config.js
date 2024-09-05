1c8333a56a574d196d2afbcf3f5df3c1ff965ed9
BillK <35489013+Bill13579@users.noreply.github.com>
2024-09-05 08:55:43 +0800

    Fix firefox specific features that were failing to activate

---
+++ firefox.webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const {DefinePlugin} = require("webpack");
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+
+module.exports = {
+    mode: "production",
+    resolve: {
+        alias: {
+            Polyfill: path.resolve(__dirname, "src/polyfill.js")
+        },
+        modules: [path.resolve(__dirname, "src"), "node_modules"]
+    },
+    entry: {
+        "background/background": "./src/background/background.js",
+        "background/captureTab": "./src/background/captureTab.js",
+        "background/tmpClear": "./src/background/tmpClear.js",
+        "background/commands": "./src/background/commands.js",
+        "popup/popup": "./src/popup/popup.js",
+        "content/content": "./src/content/content.js",
+        "options/options": "./src/options/options.js"
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new CircularDependencyPlugin({
+            exclude: /node_modules|cartographer\/pkg/,
+            failOnError: true,
+            allowAsyncCycles: false,
+            cwd: process.cwd()
+        }),
+        new DefinePlugin({
+            TARGET: "\"firefox\""
+        })
+    ],
+    experiments: {
+        asyncWebAssembly: true,
+        // buildHttp: true,
+        // layers: true,
+        // lazyCompilation: true,
+        // outputModule: true,
+        // syncWebAssembly: true,
+        topLevelAwait: true,
+    },
+    cache: false,
+};
